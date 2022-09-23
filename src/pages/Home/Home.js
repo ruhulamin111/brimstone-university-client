3301b4e4cdccb437f952031e7ec7b1ba9cd1af03
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-23 20:28:13 +0600

    deply

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Campus from '../Campus/Campus';
 import Faculty from '../Faculty/Faculty';
 import Banner from './Banner';
+import Highlights from './Highlights';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Highlights />
             <Faculty />
             <Campus />
         </div>
--- src/utilities/Loading.js
+++ src/utilities/Loading.js
@@ -3,10 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex justify-center items-center h-screen w-full'>
-            <button type="button" class="bg-indigo-500 text-white px-4 pb-2 rounded" disabled>
-                <svg class="animate-spin h-5 w-5 " viewBox="0 0 24 24">
-                </svg>
-                Processing...
+            <button type="button" class="bg-indigo-500 text-white px-4 py-2 rounded" disabled> Loading...
             </button>
         </div>
     );
