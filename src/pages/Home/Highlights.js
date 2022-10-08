70af8e0b5af61183a1f48e4c334807ff7af0837a
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-08 07:55:00 +0600

    highlight part done

--- src/pages/Home/Highlights.js
+++ src/pages/Home/Highlights.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Highlights = () => {
     return (
         <div className=' mx-auto grid lg:grid-cols-4 '>
-            <div className='py-16 px-5 bg-neutral text-center text-white'>
+            <div className='py-16 px-5 bg-primary text-center text-white'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-center">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
                 </svg>
@@ -17,27 +17,24 @@ const Highlights = () => {
                 </svg>
 
                 <h1 className='text-2xl font-medium py-5'>Tuition Fees
-
                 </h1>
-                <p>We have students coming from different backgrounds, cultures, and nationalities as well. More than 500 international students are enrolled in various programs.</p>
+                <p>We offer affordable tuition fees for students with the option to pay them in multiple installments to create convenience for students</p>
             </div>
-            <div className='py-16 px-5 bg-accent text-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-22 text-center">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+            <div className='py-16 px-5 bg-neutral text-white text-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                 </svg>
                 <h1 className='text-2xl font-medium py-5'>Scholarship
-
                 </h1>
-                <p>We have students coming from different backgrounds, cultures, and nationalities as well. More than 500 international students are enrolled in various programs.</p>
+                <p>We make sure all our students get the help they need to complete the degree. Scholarships are available for students with excellent academic results as well as for the students who are struggling financially</p>
             </div>
-            <div className='py-16 px-5 bg-primary text-center text-white'>
+            <div className='py-16 px-5 bg-primary text-center text-secondary'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-22 text-center">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
                 </svg>
-                <h1 className='text-2xl font-medium py-5'>Programs
-
+                <h1 className='text-2xl font-medium py-5 '>Programs
                 </h1>
-                <p>We have students coming from different backgrounds, cultures, and nationalities as well. More than 500 international students are enrolled in various programs.</p>
+                <p>We are offering 36 undergraduate and graduate programs under 25 departments. Our curriculum is designed to help the students to develop the skills required for 21st-century employment</p>
             </div>
         </div>
     );
