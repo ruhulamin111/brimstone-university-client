fed7162ff7f859ff14f1e68de05ded846aaf7807
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-09 22:14:46 +0600

    deploy

--- src/pages/Addmission/Addmission.js
+++ src/pages/Addmission/Addmission.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 const Addmission = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const data = {
+            student: event.type.value,
+
+        }
+        console.log('value', data);
+    }
+
     return (
         <div className='w-11/12 mx-auto bg-secondary p-8'>
-            <form className='py-5 '>
+            <form className='py-5 ' onSubmit={handleSubmit}>
                 <h1 className='my-4 pb-4 text-xl border-b-2 font-medium '>Applicant Information</h1>
                 <select className="select select-bordered w-full max-w-xs mr-10 mb-8" required>
-                    <option disabled selected>Applicant Type</option>
-                    <option>Local Student </option>
-                    <option>Internation Student</option>
+                    <option id='type' disabled selected>Applicant Type</option>
+                    <option value='1' selected='selected'>Local Student </option>
+                    <option value='2'>International Student</option>
                 </select>
                 <select className="select select-bordered w-full max-w-xs mr-10 mb-8" required>
                     <option disabled selected>Program Type</option>
--- src/utilities/Loading.js
+++ src/utilities/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='flex justify-center items-center h-screen w-full'>
-            <button type="button" class="bg-indigo-500 text-white px-4 py-2 rounded" disabled> Loading...
+            <button type="button" className="bg-indigo-500 text-white px-4 py-2 rounded" disabled> Loading...
             </button>
         </div>
     );
